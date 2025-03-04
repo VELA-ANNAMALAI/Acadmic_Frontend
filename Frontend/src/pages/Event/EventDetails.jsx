fafb06e6402b676799480c745bb644765c2f78ed
VELA-ANNAMALAI <145909520+VELA-ANNAMALAI@users.noreply.github.com>
2025-03-04 18:45:56 +0530

    Update EventDetails.jsx

--- Frontend/src/pages/Event/EventDetails.jsx
+++ Frontend/src/pages/Event/EventDetails.jsx
@@ -331,7 +331,9 @@ const EventDetails = () => {
                       alt="Event Flyer" 
                       style={{ width: '100%', maxWidth: '600px', height: 'auto' }} 
                     />
-                 
+                  </div>
+                       <Button variant="danger" onClick={handleDeleteFlyer} className="mt-2">Delete Flyer</Button>
+                     </div>
               )}
               <div className="mt-3">
                 <input 
